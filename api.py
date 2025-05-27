808a2f1028895ef06ee17d7cc9bd53828e37b6c4
Craig Willett <craig.willett@gmail.com>
2025-05-26 21:11:14 -0400

    updated requirements

--- api.py
+++ api.py
@@ -1,15 +1,7 @@
 from fastapi import FastAPI
-from rembg import remove
-from fastapi import File, UploadFile
-from fastapi.responses import Response
-
-from PIL import Image
-import io
 
 app = FastAPI()
 
-@app.post("/remove-bg")
-async def remove_bg(file: UploadFile = File(...)):
-    input_image = await file.read()
-    output_image = remove(input_image)
-    return Response(content=output_image, media_type="image/png")
+@app.get("/")
+def read_root():
+    return {"status": "ok"}
