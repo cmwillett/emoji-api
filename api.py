a73e12c7940fa401e271c1aefbb823093978b2cf
Craig Willett <craig.willett@gmail.com>
2025-05-27 09:29:56 -0400

    added logging

--- api.py
+++ api.py
@@ -13,5 +13,10 @@ def root():
 @app.post("/remove-bg")
 async def remove_bg(file: UploadFile = File(...)):
     input_bytes = await file.read()
-    output_bytes = remove(input_bytes)
-    return Response(content=output_bytes, media_type="image/png")
+
+    try:
+        output_bytes = remove(input_bytes)
+        return Response(content=output_bytes, media_type="image/png")
+    except Exception as e:
+        print("Error in background removal:", str(e))
+        return {"error": str(e)}
